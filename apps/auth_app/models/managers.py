831c7c507a4c8532896dee17245f83f66524dd2f
axeratos <nikitalukianitcares@gmail.com>
2023-05-15 21:46:06 +0300

    Managers for proxy models

---
+++ apps/auth_app/models/managers.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.models import BaseUserManager
+
+from apps.auth_app.models.base_user import User
+
+
+class JobSeekerManager(BaseUserManager):
+    def get_queryset(self, *args, **kwargs):
+        users = super().get_queryset(*args, **kwargs)
+        return users.filter(account_type=User.Types.JOBSEEKER)
+
+
+class EmployerManager(BaseUserManager):
+    def get_queryset(self, *args, **kwargs):
+        users = super().get_queryset(*args, **kwargs)
+        return users.filter(account_type=User.Types.EMPLOYER)
