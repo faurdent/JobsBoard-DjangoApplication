659f690c2a0d1385a13e16ce196947ba116d2b5e
axeratos <nikitalukianitcares@gmail.com>
2023-05-15 21:45:54 +0300

    Proxy models and separated profile models

---
+++ apps/auth_app/models/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+from apps.auth_app.models.base_user import User
+from apps.auth_app.models.managers import JobSeekerManager, EmployerManager
+
+
+class JobSeeker(User):
+    objects = JobSeekerManager()
+
+    class Meta:
+        proxy = True
+
+
+class Employer(User):
+    objects = EmployerManager()
+
+    class Meta:
+        proxy = True
+
+
+class JobSeekerProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="jobseeker_profile")
+    jobseeker_id = models.IntegerField(null=True, blank=True)
+
+
+class EmployerProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="employer_profile")
+    owner_id = models.IntegerField(null=True, blank=True)
