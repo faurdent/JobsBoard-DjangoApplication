dd9954b88e4f688e78c0e10101cb24f2c2f5e898
axeratos <nikitalukianitcares@gmail.com>
2023-05-18 16:02:52 +0300

    Add employer to the group and grant permissions

--- apps/auth_app/models/models.py
+++ apps/auth_app/models/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import Group
 from django.db import models
 
 from apps.auth_app.models.base_user import User
@@ -16,6 +17,11 @@ class Employer(User):
     objects = EmployerManager()
     base_type = User.Types.EMPLOYER
 
+    def save(self, *args, **kwargs):
+        employer: Employer = super().save(*args, **kwargs)
+        owner_group = Group.objects.get(name="Owner")
+        employer.groups.add(owner_group)
+
     class Meta:
         proxy = True
         permissions = [
@@ -25,7 +31,7 @@ class Employer(User):
 
 class JobSeekerProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="jobseeker_profile")
-    cv = models.FileField(upload_to="cv", blank=True, null=True)
+    cv = models.FileField(upload_to="cv", blank=True, null=True, default=None)
     jobseeker_id = models.IntegerField(null=True, blank=True)
     skills = models.ManyToManyField("job_board.PositionType")
 
