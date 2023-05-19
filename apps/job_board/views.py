f575afebea7fd1a56a9990f80a7893eba89afb82
axeratos <nikitalukianitcares@gmail.com>
2023-05-19 14:57:02 +0300

    Company details view

--- apps/job_board/views.py
+++ apps/job_board/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.views.generic import TemplateView, CreateView, DetailView
 
-from apps.auth_app.models import Employer, JobSeeker, JobSeekerProfile
+from apps.auth_app.models import JobSeekerProfile
 from apps.job_board.forms import CreateCompanyForm
 from apps.job_board.models import Company, Vacancy, CompanyOwnership
 
@@ -29,9 +29,11 @@ class CreateCompany(LoginRequiredMixin, PermissionRequiredMixin, CreateView):
     form_class = CreateCompanyForm
     permission_required = "job_board.add_company"
     model = Company
+
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.object = None
+
     def post(self, request, *args, **kwargs):
         form: CreateCompanyForm = self.get_form()
         if form.is_valid():
@@ -50,12 +52,22 @@ class DetailCompany(DetailView):
     model = Company
     template_name = "job_board/company_details.html"
     context_object_name = "company"
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        employer_companies = self.request.user.employer_profile.companies.all()
-        q = self.object
-        if self.object in employer_companies:
-            print("")
+        company: Company = self.object
+        employer_profile = self.request.user.employer_profile
+        if employer_profile.companies.filter(
+                owners__companyownership__company_id=company.pk
+        ).first():
+            context.update({"is_company_owner": True})
+        else:
+            context.update({"is_company_owner": False})
+        context.update({
+            "workers_count": company.workers.count(),
+            "owners_count": company.owners.count(),
+            "vacancies_count": company.vacancies.count(),
+        })
         return context
 
 
