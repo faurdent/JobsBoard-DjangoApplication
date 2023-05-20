b34703bf5837ae75f59956d4ae848b211a208e2a
axeratos <nikitalukianitcares@gmail.com>
2023-05-20 11:40:41 +0300

    Create and detail views registered

--- apps/job_board/urls.py
+++ apps/job_board/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path("create-company", views.CreateCompany.as_view(), name="create_company"),
     path("company-profile/<int:pk>", views.DetailCompany.as_view(), name="company_details"),
     path("companies", views.ViewCompanies.as_view(), name="all_companies"),
+    path("create-vacancy", views.CreateVacancy.as_view(), name="create_company"),
+    path("detail-vacancy/<int:pk>", views.DetailVacancy.as_view(), name="vacancy_details"),
 ]
