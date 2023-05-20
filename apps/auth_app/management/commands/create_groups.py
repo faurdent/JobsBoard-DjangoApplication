69549d9170d415d46d755e23709f79b5d5d2830d
axeratos <nikitalukianitcares@gmail.com>
2023-05-20 11:39:03 +0300

    Additional permissions for owners group

--- apps/auth_app/management/commands/create_groups.py
+++ apps/auth_app/management/commands/create_groups.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import Permission, Group
 from django.contrib.contenttypes.models import ContentType
 from django.core.management import BaseCommand
 
-from apps.job_board.models import Company
+from apps.job_board.models import Company, Vacancy
 
 
 class Command(BaseCommand):
@@ -12,10 +12,10 @@ class Command(BaseCommand):
     def _create_owner_group(self):
         company_content_type = ContentType.objects.get_for_model(Company)
         company_permissions = Permission.objects.filter(content_type=company_content_type)
+        vacancy_content_type = ContentType.objects.get_for_model(Vacancy)
+        vacancy_permissions = Permission.objects.filter(content_type=vacancy_content_type)
+        all_permissions = [*company_permissions, *vacancy_permissions]
         owner_group, created_owner = Group.objects.get_or_create(name="Owner")
-        if created_owner:
-            for permission in company_permissions:
-                owner_group.permissions.add(permission)
-            self.stdout.write("Company group created successfully.")
-            return
-        self.stdout.write("Company group already exists.")
+        for permission in all_permissions:
+            owner_group.permissions.add(permission)
+        self.stdout.write("Company group created successfully.")
