2380033a179ab2bab8f4f420cd85cadb90add588
axeratos <nikitalukianitcares@gmail.com>
2023-05-20 18:11:37 +0300

    Views registered

--- apps/job_board/urls.py
+++ apps/job_board/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
     path("create-company", views.CreateCompany.as_view(), name="create_company"),
     path("company-profile/<int:pk>", views.DetailCompany.as_view(), name="company_details"),
     path("companies", views.ViewCompanies.as_view(), name="all_companies"),
+    path("update-company/<int:pk>", views.UpdateCompany.as_view(), name="update_company"),
     path("create-vacancy", views.CreateVacancy.as_view(), name="create_company"),
     path("detail-vacancy/<int:pk>", views.DetailVacancy.as_view(), name="vacancy_details"),
+    path("errors/404", views.EntityNotFoundView.as_view(), name="not_found")
 ]
