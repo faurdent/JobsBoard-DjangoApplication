da652b75f7525f3ee5a9b0b35a35c645f3fa7a05
axeratos <nikitalukianitcares@gmail.com>
2023-05-26 16:56:22 +0300

    Proxy model and manager for employee user type

--- apps/auth_app/models/models.py
+++ apps/auth_app/models/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 from apps.auth_app.models.base_user import User
-from apps.auth_app.models.managers import JobSeekerManager, EmployerManager
+from apps.auth_app.models.managers import JobSeekerManager, EmployerManager, EmployeeManager
 
 
 class JobSeeker(User):
@@ -20,6 +20,14 @@ class Employer(User):
         proxy = True
 
 
+class Employee(User):
+    objects = EmployeeManager
+    base_type = User.Types.EMPLOYEE
+
+    class Meta:
+        proxy = True
+
+
 class JobSeekerProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="jobseeker_profile")
     cv = models.FileField(upload_to="cv", blank=True, null=True, default=None)
@@ -32,3 +40,9 @@ class EmployerProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="employer_profile")
     has_company = models.BooleanField(default=False)
     owner_id = models.IntegerField(null=True, blank=True)
+
+
+class EmployeeProfile(models.Model):
+    position = models.ForeignKey("job_board.PositionType", on_delete=models.PROTECT)
+    company = models.ForeignKey("job_board.Company", on_delete=models.PROTECT)
+    user = models.OneToOneField(User, on_delete=models.PROTECT)
