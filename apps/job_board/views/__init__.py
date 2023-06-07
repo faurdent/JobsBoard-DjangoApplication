79a729de39c9c4038198656cad192a4fdf48c7f7
axeratos <nikitalukianitcares@gmail.com>
2023-06-07 09:50:31 +0300

    Import all non-abstract views from package

---
+++ apps/job_board/views/__init__.py
@@ -0,0 +1,6 @@
+from .actions_views import AcceptJobSeeker, RejectJobSeeker, FireEmployeeView, VacancyResponseView
+from .company_crud_views import CreateCompany, UpdateCompany, DeleteCompany, DetailCompany, ViewCompanies
+from .company_info_views import CompanyOwnersView, EmployerCompaniesView, CompanyEmployeesView, CompanyVacanciesView
+from .error_views import EntityNotFoundView
+from .index_views import IndexEmployerView, IndexJobSeekerView
+from .vacancy_views import ViewVacancies, CreateVacancy, UpdateVacancy, DetailVacancy, ResponsesView
