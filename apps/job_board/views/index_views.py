61498c25db5e2b3056201cc381184fe36ec552a8
Axeratos <nikitalukianitcares@gmail.com>
2023-06-20 17:47:04 +0300

    Providing extra info for index page

--- apps/job_board/views/index_views.py
+++ apps/job_board/views/index_views.py
@@ -1,9 +1,21 @@
+from django.urls import reverse_lazy
+
 from apps.job_board.views.modificated_views import IndexAbstractView
 
 
 class IndexJobSeekerView(IndexAbstractView):
     template_name = "job_board/index.html"
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({"link": reverse_lazy("employer_home"), "link_redirect_name": "Employers"})
+        return context
+
 
 class IndexEmployerView(IndexAbstractView):
     template_name = "job_board/index_employer.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({"link": reverse_lazy("home"), "link_redirect_name": "Job seekers"})
+        return context
