af65ba92f2c88a45efde4dca2f84f76e9a4d22cb
Axeratos <nikitalukianitcares@gmail.com>
2023-06-26 19:21:19 +0300

    Urls on vacancy and title in view fixed

--- apps/job_board/views/vacancy_views.py
+++ apps/job_board/views/vacancy_views.py
@@ -83,7 +83,7 @@ class ViewVacancies(ListView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        context.update({"vacancies_info_list": "Vacancies"})
+        context.update({"vacancies_info_title": "Vacancies"})
         return context
 
     def get_queryset(self):
