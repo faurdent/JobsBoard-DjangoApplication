656d467db82ac8be5b4ace5a8c50cfbb502d105a
Axeratos <nikitalukianitcares@gmail.com>
2023-07-22 12:39:38 +0300

    Widgets added to user creation form. Common fields separated to another class

--- apps/auth_app/forms.py
+++ apps/auth_app/forms.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import Group
 from django.db import transaction
@@ -5,7 +6,27 @@ from django.db import transaction
 from apps.auth_app.models import Employer, JobSeeker
 
 
-class SignUpEmployerForm(UserCreationForm):
+class BaseUserCreationForm(UserCreationForm):
+    password1 = forms.CharField(
+        label="Password",
+        widget=forms.PasswordInput(
+            attrs={'class': 'form-control', 'type': 'password'}),
+    )
+    password2 = forms.CharField(
+        label="Confirm password",
+        widget=forms.PasswordInput(
+            attrs={'class': 'form-control', 'type': 'password'}),
+    )
+
+    class Meta:
+        fields = ('username', 'email', 'password1', 'password2')
+        widgets = {
+            "username": forms.TextInput(attrs={"class": "form-control"}),
+            "email": forms.EmailInput(attrs={"class": "form-control"}),
+        }
+
+
+class SignUpEmployerForm(BaseUserCreationForm):
     def save(self, commit=True):
         with transaction.atomic():
             employer = super().save(commit)
@@ -13,12 +34,10 @@ class SignUpEmployerForm(UserCreationForm):
             employer.groups.add(owner_group)
             return employer
 
-    class Meta:
+    class Meta(BaseUserCreationForm.Meta):
         model = Employer
-        fields = ('username', 'email', 'password1', 'password2')
 
 
-class SignUpJobSeekerForm(UserCreationForm):
-    class Meta:
+class SignUpJobSeekerForm(BaseUserCreationForm):
+    class Meta(BaseUserCreationForm.Meta):
         model = JobSeeker
-        fields = ('username', 'email', 'password1', 'password2')
