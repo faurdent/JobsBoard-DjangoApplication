17ac2998b1f9469508b9bef2231ee821c29d9a6a
Axeratos <nikitalukianitcares@gmail.com>
2023-07-25 08:51:26 +0300

    Logo field added to company model

--- apps/auth_app/migrations/0012_user_avatar.py
+++ apps/auth_app/migrations/0012_user_avatar.py
@@ -14,6 +14,6 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='user',
             name='avatar',
-            field=models.ImageField(blank=True, max_length=255, null=True, upload_to=apps.services.image_paths.ImagePathsGenerator.get_avatar_path_user),
+            field=models.ImageField(blank=True, max_length=255, null=True, upload_to=apps.services.image_paths.ImagePathsGenerator.get_user_avatar_path),
         ),
     ]
