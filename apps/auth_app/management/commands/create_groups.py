76f23f91fafe9550c4a17ad3e6f56b698d56cea6
Axeratos <nikitalukianitcares@gmail.com>
2023-08-08 10:12:53 +0300

    HR group created, command for groups updated

--- apps/auth_app/management/commands/create_groups.py
+++ apps/auth_app/management/commands/create_groups.py
@@ -1,23 +1,30 @@
+from typing import Type
+
 from django.contrib.auth.models import Permission, Group
 from django.contrib.contenttypes.models import ContentType
 from django.core.management import BaseCommand
+from django.db.models import Model
 
+from apps.auth_app.models.models import Employee
 from apps.job_board.models import Company, Vacancy, VacancyResponse
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        self._create_owner_group()
+        self._create_group("Owner", [VacancyResponse, Vacancy, Company, Employee])
+        self._create_group("HR", [Vacancy, Employee])
 
-    def _create_owner_group(self):
-        company_content_type = ContentType.objects.get_for_model(Company)
-        company_permissions = Permission.objects.filter(content_type=company_content_type)
-        vacancy_content_type = ContentType.objects.get_for_model(Vacancy)
-        vacancy_permissions = Permission.objects.filter(content_type=vacancy_content_type)
-        vacancy_response_type = ContentType.objects.get_for_model(VacancyResponse)
-        vacancy_responses_permissions = Permission.objects.filter(content_type=vacancy_response_type)
-        all_permissions = [*company_permissions, *vacancy_permissions, *vacancy_responses_permissions]
-        owner_group, created_owner = Group.objects.get_or_create(name="Owner")
+    def _create_group(self, group_name: str, models: list[Type[Model]]):
+        all_permissions = []
+        for model in models:
+            all_permissions.extend(self.get_permissions_for_model(model, group_name))
+        owner_group, created_owner = Group.objects.get_or_create(name=group_name)
         for permission in all_permissions:
             owner_group.permissions.add(permission)
-        self.stdout.write("Company group created successfully.")
+        self.stdout.write(f"{group_name} group created successfully.")
+
+    def get_permissions_for_model(self, model: Type[Model], group_name: str):
+        content_type = ContentType.objects.get_for_model(model)
+        permissions = Permission.objects.filter(content_type=content_type)
+        self.stdout.write(f"Got permissions for {group_name} group and model {model._meta.model_name}")
+        return permissions
